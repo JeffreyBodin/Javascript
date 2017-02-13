c2eddd44af5335b6c3cd46c45291f43ed77b2cfd
Jeffrey Bodin <jeffreybodin713@gmail.com>
2017-02-13 17:25:38 -0600

    notes

--- Notes.js
+++ Notes.js
@@ -6,3 +6,23 @@ We can find the remainder after dividing two numbers with a modulus: %.
 We can generate a random number with Math.random, then make it a whole number with Math.floor.
 We write a single line comment with // and a multi-line comment with /* and */
 */
+// String interpolation- is when you insert a variable into a string
+
+ 
+//Variable Notes
+
+var myName = 'Arya';
+console.log(myName);
+/*
+var, short for variable, is the JavaScript keyword that will create a new variable for us.
+
+myName is chosen by a developer. Notice that the word has no spaces, and each new word is capitalized. 
+This is a common convention in JavaScript, and is called camelCase.
+
+= means to assign whatever's next to the variable.
+
+'Arya' is the value that the equals = assigns into the variable myName.
+
+After the variable is declared, we can print the variable with: console.log(myName).
+This will print 'Arya' to the console.
+*/
