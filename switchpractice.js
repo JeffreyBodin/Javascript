8142cdc4d39c38f5792d44185f245099700ca5c0
Jeffrey Bodin <jeffreybodin713@gmail.com>
2017-02-14 16:37:27 -0600

    created a switch practice problem

---
+++ switchpractice.js
@@ -0,0 +1,11 @@
+// Switch Examples
+var pwnNoobs = true;
+
+switch (pwnNoobs) {
+    case false:
+      console.log('Noob is not pwned :(');
+      break;
+    case true:
+      console.log('Noob is pwned! :)');
+      break;
+}
