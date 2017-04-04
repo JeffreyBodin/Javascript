f909be1774303a1b086b48970b7c0a83eee17bb8
Jeffrey Bodin <jeffreybodin713@gmail.com>
2017-04-03 22:40:14 -0500

    commit

---
+++ Projects/snoowrap test app/notesSnoowrap.js
@@ -0,0 +1,2 @@
+// Note this https://github.com/not-an-aardvark/reddit-oauth-helper
+// For setting up authentication of reddit accounts was really helpful.
---
+++ Projects/snoowrap test app/snoowrapApp.js
@@ -0,0 +1,9 @@
+const snoowrap = require('snoowrap');
+
+
+const r = new snoowrap({
+    userAgent: 'myApp v1.0.0 by /u/waldytest',
+    clientId: '',
+    clientSecret: '',
+    username: 'waldytest',
+})
