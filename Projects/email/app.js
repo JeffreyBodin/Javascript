28004c53ad80c7db8d9fdcbd7e844dce89259457
Jeffrey Bodin <jeffreybodin713@gmail.com>
2017-12-01 00:43:53 -0600

    commit

---
+++ Projects/email/app.js
@@ -0,0 +1,7 @@
+window.open('mailto:test@example.com')
+
+var recipient="test";
+var at = String.fromCharCode(64);
+var dotcom="example.com";
+var mail="mailto:";
+window.open(mail+recipient+at+dotcom);
---
+++ visualstudiocode preferences.js
@@ -0,0 +1,29 @@
+{
+    "editor.tabSize": 4,
+    "files.autoSave": "afterDelay",
+    "editor.fontSize": 16,
+    "editor.fontFamily": "Consolas, 'Courier New', monospace, Helvetica",
+    "window.restoreWindows": "all",
+    "window.restoreFullscreen": true,
+    "window.newWindowDimensions": "inherit",
+    "window.menuBarVisibility": "visible",
+    "window.zoomLevel": 1.5,
+    "files.hotExit": "onExitAndWindowClose",
+    "npm.runInTerminal": true,
+    "npm.useRootDirectory": true,
+    "npm.autoDetect": "on",
+    "git.enabled": true,
+    "git.enableSmartCommit": true,
+    "terminal.integrated.shell.windows": "C:\\Windows\\Sysnative\\WindowsPowerShell\\v1.0\\powershell.exe",
+    "workbench.statusBar.visible": false,
+    "workbench.activityBar.visible": true,
+    "eslint.enable": true,
+    "eslint.run": "onType",
+    "eslint.autoFixOnSave": false,
+    "workbench.editor.openPositioning": "right",
+    "eslint.validate": [
+        "javascript",
+        "javascriptreact"
+    ],
+    "editor.minimap.enabled": false
+}
