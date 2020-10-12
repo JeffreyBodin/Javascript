87565a7251d4130a5fe5c9c7871bfa317061dc0d
Jeffrey Bodin <jeffreybodin@outlook.com>
2020-10-11 23:17:06 -0500

    2020-10-11 - javascript - moved folders

---
+++ Scripts/StringAdd.js
@@ -0,0 +1,11 @@
+var stringAdd = (outPutVariableNames, inputStrings) => {
+  let varsName = outPutVariableNames;
+  let newString = inputStrings;
+  let nameVar = () => {
+    let var1 = varsName;
+    return var1
+  }
+  let varX = nameVar();
+  let stringsSum = varX += newString;
+  return stringsSum;
+}
