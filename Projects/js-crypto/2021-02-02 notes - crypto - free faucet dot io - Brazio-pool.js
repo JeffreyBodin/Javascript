85e639fcadaf1b68947b92f798e7090adb85d021
Jeffrey Bodin <jeffreybodin@outlook.com>
2021-02-02 05:25:11 -0600

    2021-02-02_0515 - JeffreyBodin - Javascript - Projects - js-crypto - initial commit / JeffreyBodin /   / Javascript /     / Projects /       / js-crypto / - added "2021-02-02 notes - crypto - free faucet dot io - Brazio-pool.js" - added "LICENSE" -- no actual text though -- or contents -- in spite above ^   -> "2021-02-02 notes - crypto - free faucet dot io - Brazio-pool.js"   -- LICENSED w       -> "MIT"   -- at       -> bottom of document || end on document       -> "v.0.0.1"

---
+++ Projects/js-crypto/2021-02-02 notes - crypto - free faucet dot io - Brazio-pool.js
@@ -0,0 +1,79 @@
+2021-02-02 notes - crypto - free faucet dot io - Brazio-pool
+
+
+// 2021-02-02
+// free faucet dot io - Brazio-pool
+
+// https://freefaucet.io/brazio_pool/
+
+
+// MASTER Objects || Trees
+var Previouswinners = { };
+
+
+
+
+// Vars
+var winnerStringLn = '' +
+  TraderTrev 2784.800 BRAZ
+  + ' ' + '|' + ' ' + 
+  dazablac 12701.600 BRAZ
+  + ' ' + '|' + ' ' + 
+  marlontrader1 5496.800 BRAZ
+  + ' ' + '|' + ' ' + 
+  TraderTrev	13632.000 BRAZ
+  + ' ' + '|' + ' ' + 
+  TraderTrev	6350.400 BRAZ
+  + ' ' + '|' + ' ' + 
+  sroe 6393.600 BRAZ
+  + ' ' + '|' + ' ' + 
+  TraderTrev	4475.200 BRAZ
+  + ' ' + '|' + ' ' + 
+  TraderTrev	5054.400 BRAZ
+  + ' ' + '|' + ' ' + 
+  TraderTrev 11696.000 BRAZ
+  + ' ' + '|' + ' ' + 
+  TraderTrev 5491.200 BRAZ
+
+var winnerList = {
+  // waldy713: {name: 'waldy713', wonT10Wks: 0, wonT10WksWonString: 'waldy713 1337.000 BRAZ' }
+  TraderTrev: {name: 'TraderTrev'},
+  dazablac: {name: 'dazablac'},
+  marlontrader1: {name: 'marlontrader1'},
+  sroe: {name: 'sroe'}
+}
+
+// Objects
+var byWeek = { 
+  1:  'TraderTrev 2,784.800 BRAZ',
+  2:  'dazablac 12,701.600 BRAZ',
+  3:  'marlontrader1 5,496.800 BRAZ',
+  4:  'TraderTrev	13,632.000 BRAZ',
+  5:  'TraderTrev	6,350.400 BRAZ',
+  6:  'sroe 6,393.600 BRAZ',
+  7:  'TraderTrev	4,475.200 BRAZ',
+  8:  'TraderTrev	5,054.400 BRAZ',
+  9:  'TraderTrev	11,696.000 BRAZ',
+  10: 'TraderTrev	5,491.200 BRAZ'
+},
+
+var byWinner = {
+  winnerStringLn: winnerStringLn,
+},
+
+
+Previouswinners = Previouswinners +  byWeek + byWinner
+
+
+
+
+// LICENSE //
+/* 
+js-crypto - 2021-02-02 notes - crypto - free faucet dot io - Brazio-pool.js
+2021-02-02
+v.0.0.1
+
+MIT License Copyright (c)
+
+2021 Jeffrey Bodin
+*/
