d8ee7d4f1c05d3965f0e744a30e7d2d21924f2d9
Jeffrey Bodin <jeffreybodin713@gmail.com>
2021-07-20 18:26:46 -0500

    Create 2021-07-20 IFTTTT - Coinbase - ALGO webhook_Buy.js
    2021-07-20_1823
    - javascript 
    - ifttt
    - jeffreybodin713@gmail.com
    - initial
    -- new repo
    -- "ifttt-javascript"
    - For
    -- IFTTT Scripts && Webhook Calls
    - See "url"
    -> https://ifttt.com/applets/MUzmi2k9
    - This is for Coinbase
    - For a "Buy" to "Email" to "Price" && "Amount" && "Time"
    - For any given Crypto Currency
    - 'this' is ALGO

---
+++ Projects/ifttt-javascript/2021-07-20 IFTTTT - Coinbase - ALGO webhook_Buy.js
@@ -0,0 +1,13 @@
+// 2021-07-20
+  // 2021-07-20 IFTTTT - Coinbase - ALGO webhook_Buy
+
+// IFTTT
+// Coinbase -> ALGO -> EMAIL HTTPS Webhook
+
+New buy: {{CryptocurrencyCode}} of {{CryptcourrencyAmount}}
+
+{{PayoutAt}}
+of
+{{CryptcourrencyAmount}}
+in
+{{Wallet}}
